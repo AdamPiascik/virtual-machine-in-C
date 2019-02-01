573da416632514518973a0b54084107ec9303de8
AdamPiascik <43042327+AdamPiascik@users.noreply.github.com>
2019-02-01 13:02:04 +0000

    changed test programs

--- programs.c
+++ programs.c
@@ -1,25 +1,31 @@
 #include "myVM.h"
 
+/*  These are some test programs, which will eventually be moved
+    into separate text files. */
+
 const int program1[] = {
-    PSH, 9,
-    PSH, 5,
-    ADD,
+    PSH, 10,
+    PSH, 2,
+    SUB,
     POP,
     HLT
 };
+size_t program1_length = sizeof(program1) / sizeof(program1[0]);
 
 const int program2[] = {
     PSH, 10,
-    PSH, 5,
-    DIV,
+    PSH, 2,
+    MOD,
     POP,
     HLT
 };
+size_t program2_length = sizeof(program2) / sizeof(program2[0]);
 
 const int program3[] = {
-    PSH, 9,
-    PSH, 9,
-    ADD,
+    PSH, 10,
+    PSH, 2,
+    NEG,
     POP,
     HLT
 };
+size_t program3_length = sizeof(program3) / sizeof(program3[0]);
