f7d0934c878786f341ec83e325d9c371aafd7bcc
AdamPiascik <43042327+AdamPiascik@users.noreply.github.com>
2019-02-02 20:20:58 +0000

    renamed files and moved makeInstruction into its own file

---
+++ makeInstruction.c
@@ -0,0 +1,35 @@
+#include "myVM.h"
+
+int makeInstruction(char *program_line)
+{
+    if (strcmp(program_line, "PSH,\n") == 0){
+        return PSH;
+    }
+    else if (!strcmp(program_line, "POP,\n")){
+        return POP;
+    }
+    else if (!strcmp(program_line, "ADD,\n")){
+        return ADD;
+    }
+    else if (!strcmp(program_line, "SUB,\n")){
+        return SUB;
+    }
+    else if (!strcmp(program_line, "MOD,\n")){
+        return MOD;
+    }
+    else if (!strcmp(program_line, "DIV,\n")){
+        return DIV;
+    }
+    else if (!strcmp(program_line, "MULT,\n")){
+        return MULT;
+    }
+    else if (!strcmp(program_line, "NEG,\n")){
+        return NEG;
+    }
+    else if (!strcmp(program_line, "HLT")){
+        return HLT;
+    }
+    else{
+        return -1;
+    }
+}
